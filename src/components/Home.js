f9772219dd247a1b6a2f1b6b31f53c06e0d435ef
ReligaP <ReligaP@interia.pl>
2021-12-27 23:57:52 +0100

    Add changes to App.js and Home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import { useEffect , useState } from 'react';
+import { getAuth , onAuthStateChanged } from "firebase/auth";
+import app from "../firebase/firebaseconfig";
 import HomeHeader from "./HomeHeader";
 import HomeThreeColumns from "./HomeThreeColumns";
 import HomeSimpleSteps from "./HomeSimpleSteps";
 import HomeAbout from "./HomeAbout";
 import HomeWhoWeHelp from "./HomeWhoWeHelp";
-import HomeContact from "./HomeContant";
+import HomeContact from "./HomeContact";
+
 
 
 const Home = () => {
+    const [email, setEmail] = useState("");
+    useEffect(() => {
+        const auth = getAuth(app);
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log(user.email);
+                setEmail(user.email);
+            }
+            else {
+                console.log("Użytkownik wylogowany")
+                setEmail("");
+            }
+        });
+    }, []);
     return (
         <>
-            <HomeHeader />
+            <HomeHeader email={email}/>
             <HomeThreeColumns />
             <HomeSimpleSteps />
             <HomeAbout />
