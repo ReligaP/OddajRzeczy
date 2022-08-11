9ee76933a5edecfff1045fa91c7821ba65eb51d5
ReligaP <ReligaP@interia.pl>
2022-08-11 18:49:44 +0200

    Add MobileRegister.js and _mobileRegister.scss

---
+++ src/components/MobileView/MobileRegister.js
@@ -0,0 +1,70 @@
+
+const MobileRegister = (props) => {
+
+    return (
+        <div className="mobileRegisterBox">
+            <div className="mobileSignInBox_title">
+                <p className="mobileSignInBox_title__content">
+                    Załóż konto
+                </p>
+            </div>
+            <form
+                className="mobileSignInBox_form"
+                onSubmit={props.onClick}
+            >
+                <div className="mobileSignInBox_form__content">
+                    <div className="mobileSignInBox_input">
+                        <label className="mobileSignInBox_input__label">
+                            Email
+                        </label>
+                        <input
+                            className={`loginBox_input__field ${props.errors.email ? `registerError_field` : ``} `}
+                            type="email"
+                            {...props.register("email")}
+                        />
+                        <p className="registerError mobileError">
+                            {props.errors.email?.message}
+                        </p>
+                    </div>
+                    <div className="mobileSignInBox_input">
+                        <label className="mobileSignInBox_input__label">
+                            Hasło
+                        </label>
+                        <input
+                            className={`loginBox_input__field ${props.errors.password ? `registerError_field` : ``} `}
+                            type="password"
+                            {...props.register("password")}
+                        />
+                        <p className="registerError">
+                            {props.errors.password?.message}
+                        </p>
+                    </div>
+                    <div className="mobileSignInBox_input">
+                        <label className="mobileSignInBox_input__label">
+                            Powtórz Hasło
+                        </label>
+                        <input
+                            className={`loginBox_input__field ${props.errors.confirmPassword ? `registerError_field` : ``} `}
+                            type="password"
+                            {...props.register("confirmPassword")}
+                        />
+                        <p className="registerError">
+                            {props.errors.confirmPassword?.message}
+                        </p>
+                        {
+                            Boolean(props.invalidEmail) && <p className="invalidEmail">Adres email zajęty</p>
+                        }
+                    </div>
+                    <button
+                        className="mobileSignInBox_button mobileRegisterBox_button"
+                        type="submit"
+                    >
+                        Załóż konto
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default MobileRegister;
