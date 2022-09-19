939da9b02d14d480aed8a5245024def06afe2468
ReligaP <ReligaP@interia.pl>
2022-09-19 12:09:16 +0200

    Add mobileFormFund.js and mobileFormCityOptions.js to database

---
+++ src/database/mobileFormCityOptions.js
@@ -0,0 +1,3 @@
+const mobileFormCityOptions = ["Warszawa","Kraków","Poznań","Wrocław","Katowice","Gdańsk","Lublin"];
+
+export default mobileFormCityOptions;
---
+++ src/database/mobileFormFund.js
@@ -0,0 +1,207 @@
+const mobileFormFund = [
+            {
+                "city": "Warszawa",
+                "title": "Fundacja Lorem Ipsum",
+                "type" : ["dzieciom"],
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                                " Curabitur faucibus purus et efficitur egestas. Mauris hendrerit fringilla" +
+                                " hendrerit. Aliquam ac ipsum id ligula interdum malesuada.",
+                "id": 1
+            },
+            {
+                "city": "Warszawa",
+                "title": "Fundacja Pretium Nisi",
+                "type": ["samotnym matkom","dzieciom"],
+                "description": "Proin ac quam mattis, pellentesque quam sed, " +
+                                "euismod magna. Phasellus interdum id nibh at consequat. " +
+                                "Nam ullamcorper vulputate varius. ",
+                "id": 2
+            },
+            {
+                "city": "Warszawa",
+                "title": "Fundacja Platea Dictumst",
+                "type": ["niepełnosprawnym"],
+                "description": "Praesent consequat, elit eu dictum ultricies, massa nisi " +
+                                "condimentum nunc, eget tincidunt ipsum velit sed nisi.",
+                "id": 3
+            },
+            {
+                "city": "Warszawa",
+                "title": "Fundacja Sem Fringilla",
+                "type": ["bezdomnym"],
+                "description": "Nulla sit amet nisi et felis posuere tempor. Nam pellentesque " +
+                                "vehicula nisi, non commodo dolor venenatis id. ",
+                "id": 4
+            },
+            {
+                "city": "Warszawa",
+                "title": "Fundacja Aptent Taciti",
+                "type": ["bezdomnym","niepełnosprawnym"],
+                "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+                "id": 5
+            },
+            {
+                "city": "Kraków",
+                "title": "Fundacja Lorem Suscipit",
+                "type": ["bezdomnym","niepełnosprawnym"],
+                "description": "Vivamus porta purus et ultrices venenatis. Integer scelerisque ullamcorper efficitur.",
+                "id": 6
+            },
+            {
+                "city": "Kraków",
+                "title": "Fundacja Quisque Rutrum",
+                "type": ["samotnym matkom"],
+                "description": "Nulla tellus velit, sagittis a dolor sed, pretium tincidunt elit." +
+                                " Sed fermentum sagittis felis, et tincidunt ligula fermentum a. " +
+                                "In hac habitasse platea dictumst.",
+                "id": 7
+            },
+            {
+                "city": "Kraków",
+                "title": "Fundacja Rutrum Finibus",
+                "type": ["samotnym matkom","dzieciom"],
+                "description": "Quisque rutrum pellentesque velit, ut accumsan erat molestie nec." +
+                                " Vestibulum ac nisi eget ante efficitur suscipit.",
+                "id": 8
+            },
+            {
+                "city": "Kraków",
+                "title": "Fundacja Donec Metus",
+                "type": ["samotnym matkom","dzieciom","niepełnosprawnym"],
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                                "Mauris sollicitudin accumsan lorem vitae suscipit.",
+                "id": 9
+            },
+            {
+                "city": "Kraków",
+                "title": "Fundacja Lorem Curabitur",
+                "type": ["dzieciom","niepełnosprawnym","bezdomnym"],
+                "description": "Duis at efficitur dolor. Donec non eleifend augue." +
+                                " Morbi nulla libero, cursus nec lectus id, cursus euismod sapien.",
+                "id": 10
+            },
+            {
+                "city": "Wrocław",
+                "title": "Fundacja Mauris Facilisis",
+                "type": ["dzieciom","niepełnosprawnym","bezdomnym","samotnym matkom"],
+                "description": "Quisque suscipit ante at lectus rutrum luctus. Cras a auctor tellus." +
+                                " Etiam vehicula at justo quis posuere. Nam a tellus eget nunc ultrices " +
+                                "euismod et eget nibh.",
+                "id": 11
+            },
+            {
+                "city": "Wrocław",
+                "title": "Fundacja Feugiat Sem",
+                "type": ["dzieciom","niepełnosprawnym"],
+                "description": "Nunc gravida, erat in commodo ultrices, erat urna egestas ante, " +
+                                "vitae tempor urna urna eget massa. ",
+                "id": 12
+            },
+            {
+                "city": "Wrocław",
+                "title": "Fundacja Sed Placerat",
+                "type": ["bezdomnym"],
+                "description": "Class aptent taciti sociosqu ad litora torquent per conubia nostra, inceptos himenaeos.",
+                "id": 13
+            },
+            {
+                "city": "Katowice",
+                "title": "Fundacja Magna Vulputate" ,
+                "type": ["niepełnosprawnym","bezdomnym"],
+                "description": "Nullam nibh diam, eleifend at posuere sed, auctor sed massa.",
+                "id": 14
+            },
+            {
+                "city": "Katowice",
+                "title": "Fundacja Nullam Efficitur",
+                "type": ["dzieciom","samotnym matkom"],
+                "description": "Nunc gravida, erat in commodo ultrices, erat urna egestas ante, " +
+                                "vitae tempor urna urna eget massa.",
+                "id": 15
+            },
+            {
+                "city": "Katowice",
+                "title": "Fundacja  Faucibus Euismod" ,
+                "type": ["samotnym matkom"],
+                "description": "Nunc vitae tempus neque. Curabitur eget erat id est scelerisque pulvinar quis vitae ante.",
+                "id": 16
+            },
+            {
+                "city": "Poznań",
+                "title": "Fundacja Donec Vitae" ,
+                "type": ["samotnym matkom","dzieciom"],
+                "description": "Morbi lectus mauris, fringilla et condimentum ut, condimentum sit amet nibh.",
+                "id": 17
+            },
+            {
+                "city": "Poznań",
+                "title": "Fundacja Etiam Eget" ,
+                "type": ["samotnym matkom","niepełnosprawnym"],
+                "description": "Mauris feugiat augue pulvinar sapien commodo, eget sodales ligula hendrerit. " +
+                    "           Sed quis mattis orci",
+                "id": 18
+            },
+            {
+                "city": "Poznań",
+                "title": "Fundacja Aenean Interdum" ,
+                "type": ["bezdomnym"],
+                "description": "Curabitur porta arcu in viverra efficitur. Integer consectetur " +
+                                "sollicitudin justo sit amet vestibulum.",
+                "id": 19
+            },
+            {
+                "city": "Gdańsk",
+                "title": "Fundacja Suspendisse" ,
+                "type": ["samotnym matkom","dzieciom"],
+                "description": "Suspendisse tempor, augue non posuere finibus, mi nibh congue magna, " +
+                                "quis bibendum urna dui non lectus.",
+                "id": 20
+            },
+            {
+                "city": "Gdańsk",
+                "title": "Fundacja Convallis Lacus" ,
+                "type": ["dzieciom","niepełnosprawnym"],
+                "description": "Vestibulum nec sapien nec massa vulputate varius quis sit amet velit. " +
+                                "Maecenas pharetra massa nec ex porttitor egestas.",
+                "id": 21
+            },
+            {
+                "city": "Gdańsk",
+                "title": "Fundacja Duis Tempus" ,
+                "type": ["bezdomnym"],
+                "description": "Maecenas semper leo id sapien congue, vel euismod risus dapibus.",
+                "id": 22
+            },
+            {
+                "city": "Gdańsk",
+                "title": "Fundacja Vestibulum" ,
+                "type": ["bezdomnym","niepełnosprawnym"],
+                "description": "Phasellus nunc enim, porttitor eu varius ac, ultricies ut nibh. Aliquam congue" +
+                                "diam in laoreet hendrerit. ",
+                "id": 23
+            },
+            {
+                "city": "Lublin",
+                "title": "Fundacja Nullam Molestie" ,
+                "type": ["dzieciom","niepełnosprawnym","bezdomnym","samotnym matkom"],
+                "description": "Nunc ut urna purus. Duis elementum urna nibh, vitae dictum lacus fringilla sit amet.",
+                "id": 24
+            },
+            {
+                "city": "Lublin",
+                "title": "Fundacja Dapibus Pulvinar" ,
+                "type": ["dzieciom","niepełnosprawnym","samotnym matkom"],
+                "description": "Mauris feugiat augue pulvinar sapien commodo, eget " +
+                            "sodales ligula hendrerit. Sed quis mattis orci.",
+                "id": 25
+            },
+            {
+                "city": "Lublin",
+                "title": "Fundacja Faucibus Euismod" ,
+                "type": ["bezdomnym"],
+                "description": "Etiam suscipit elit nec nisi malesuada pellentesque. Nullam suscipit malesuada rutrum.",
+                "id": 26
+            },
+        ]
+
+export default mobileFormFund;
