eeb62617c79d396dc2ddd81bd8ca890b63a6a125
ReligaP <ReligaP@interia.pl>
2022-09-29 15:42:56 +0200

    Add semantic tags and cleanup in MobileHeader.js

--- src/components/MobileView/MobileHeader.js
+++ src/components/MobileView/MobileHeader.js
@@ -2,10 +2,9 @@ import { Link } from "react-router-dom";
 import { getAuth , signOut } from "firebase/auth";
 import app from "../../firebase/firebaseconfig";
 
-
 const MobileHeader = (props) => {
 
-    const ClickHandler = () => {
+    const clickHandlerSignOut = () => {
         const auth=getAuth(app);
         signOut(auth).then(() => {
             console.log("Wylogowany");
@@ -14,64 +13,68 @@ const MobileHeader = (props) => {
             const errorMessage = error.message;
             console.log(errorCode, errorMessage);
         })
-    }
-
+    };
     return (
-        <div className="mobileHeaderBox">
-            <div className="mobileHeaderBox_logo">
-                <Link className="mobileHeaderBox_logo__link" to="/">
-                    <p className="mobileHeaderBox_logo__content">
+        <header>
+            <div className="mobileHeaderBox">
+                <div className="mobileHeaderBox_logo">
+                    <p className="mobileHeaderBox_logo__text mobileLogoUpperText">
                         Oddaj
                     </p>
-                    <p className="mobileHeaderBox_logo__content">
+                    <p className="mobileHeaderBox_logo__text">
                         &nbsp;&nbsp;Rzeczy
                     </p>
-                </Link>
-            </div>
-            <div className="mobileHeaderBox_menu">
-                <input type="checkbox" className="check" />
-                <ul className="menu-items">
-                    <li>
-                        <Link to="/">
-                            Strona główna
-                        </Link>
-                    </li>
-                    {
-                        props.email ?
-                            <>
-                                <li>
-                                    <Link to="/oddaj-rzeczy">
-                                        Oddaj rzeczy
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link  onClick={ClickHandler} to="/">
-                                        Wyloguj się
-                                    </Link>
-                                </li>
-                            </>
-                            :
-                            <>
-                                <li>
-                                    <Link to="/logowanie">
-                                        Zaloguj się
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="/rejestracja">
-                                        Załóż konto
-                                    </Link>
-                                </li>
-                            </>
-                    }
-                </ul>
-                <div className="ham-menu">
-                    <span className="line line1" />
-                    <span className="line line2" />
-                    <span className="line line3" />
                 </div>
+                <nav>
+                    <div className="mobileHeaderBox_menu">
+                        <input
+                            type="checkbox"
+                            className="check"
+                        />
+                        <ul className="menu-items">
+                            <li>
+                                <Link to="/">
+                                    Strona główna
+                                </Link>
+                            </li>
+                            {
+                                props.email ?
+                                    <>
+                                        <li>
+                                            <Link to="/oddaj-rzeczy">
+                                                Oddaj rzeczy
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link  onClick={clickHandlerSignOut} to="/">
+                                                Wyloguj się
+                                            </Link>
+                                        </li>
+                                    </>
+                                    :
+                                    <>
+                                        <li>
+                                            <Link to="/logowanie">
+                                                Zaloguj się
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/rejestracja">
+                                                Załóż konto
+                                            </Link>
+                                        </li>
+                                    </>
+                            }
+                        </ul>
+                        <div className="ham-menu">
+                            <span className="line line1" />
+                            <span className="line line2" />
+                            <span className="line line3" />
+                        </div>
+                    </div>
+                </nav>
             </div>
-        </div>
+        </header>
     );
 };
 
