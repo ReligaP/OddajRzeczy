89a71f01d8f2d259a755b0db7458244e2d49aeff
ReligaP <ReligaP@interia.pl>
2022-11-16 20:18:31 +0100

    Create Loading.js component, _loading.scss and add styles to main.scss

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+
+const Loading = () => {
+    return (
+        <main>
+            <section className="loadingBox">
+                <p>
+                    Wczytywanie...
+                </p>
+            </section>
+        </main>
+    );
+};
+
+export default Loading;
