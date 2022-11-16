dc92e5d0790e66dedfbe848b881a6e67b95f0c14
ReligaP <ReligaP@interia.pl>
2022-11-16 20:23:59 +0100

    Implement lazy loading to App.js component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes , Route } from "react-router";
-import Home from "./components/Home";
-import SignIn from "./components/SignIn";
-import Register from "./components/Register";
-import SignOut from "./components/SignOut";
-import Form from "./components/Form/Form";
+import { Suspense , lazy } from "react";
+import Loading from "./components/Loading";
+const HomeSuspense = lazy(() => import('./components/Home'));
+const SignInSuspense = lazy(() => import('./components/SignIn'));
+const RegisterSuspense = lazy(() => import('./components/Register'));
+const SignOutSuspense = lazy(() => import('./components/SignOut'));
+const FormSuspense = lazy(() => import('./components/Form/Form'));
 
 function App() {
-  return(
-      <BrowserRouter>
-        <Routes>
-            <Route exact path="/" element={<Home />}/>
-            <Route exact path="/logowanie" element={<SignIn />}/>
-            <Route exact path="/rejestracja" element={<Register />}/>
-            <Route exact path="/wylogowano" element={<SignOut />}/>
-            <Route exact path="/oddaj-rzeczy" element={<Form />}/>
-        </Routes>
-      </BrowserRouter>
-  );
+    return(
+        <BrowserRouter>
+            <Suspense fallback={<Loading />}>
+                <Routes>
+                    <Route exact path="/" index element={<HomeSuspense />}/>
+                    <Route exact path="/logowanie" element={<SignInSuspense />}/>
+                    <Route exact path="/rejestracja" element={<RegisterSuspense />}/>
+                    <Route exact path="/wylogowano" element={<SignOutSuspense />}/>
+                    <Route exact path="/oddaj-rzeczy" element={<FormSuspense />}/>
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
 }
+
 export default App;
