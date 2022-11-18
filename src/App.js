05487a314aa6cd03bd3f7d17c0416485c74dfd21
ReligaP <ReligaP@interia.pl>
2022-11-18 09:47:57 +0100

    Implement private routing to App.js component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
-import { Routes , Route } from "react-router";
-import { Suspense , lazy } from "react";
+import { Routes , Route , Navigate } from "react-router";
+import { Suspense , lazy , useState , useEffect } from "react";
+import { getAuth , onAuthStateChanged } from "firebase/auth";
 import Loading from "./components/Loading";
+import app from "./firebase/firebaseconfig";
+
 const HomeSuspense = lazy(() => import('./components/Home'));
 const SignInSuspense = lazy(() => import('./components/SignIn'));
 const RegisterSuspense = lazy(() => import('./components/Register'));
@@ -9,6 +12,29 @@ const SignOutSuspense = lazy(() => import('./components/SignOut'));
 const FormSuspense = lazy(() => import('./components/Form/Form'));
 
 function App() {
+    const [email,setEmail] = useState("");
+    useEffect(() => {
+        const auth = getAuth(app);
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setEmail(user.email);
+            }
+            else {
+                console.log("Użytkownik wylogowany")
+                setEmail("");
+            }
+        });
+    }, []);
+    const ProtectedRoute = ({
+                                email,
+                                redirectPath = '/',
+                                children,
+                            }) => {
+        if (email === "") {
+            return <Navigate to={redirectPath} replace />;
+        }
+        return children;
+    };
     return(
         <BrowserRouter>
             <Suspense fallback={<Loading />}>
@@ -16,8 +42,10 @@ function App() {
                     <Route exact path="/" index element={<HomeSuspense />}/>
                     <Route exact path="/logowanie" element={<SignInSuspense />}/>
                     <Route exact path="/rejestracja" element={<RegisterSuspense />}/>
-                    <Route exact path="/wylogowano" element={<SignOutSuspense />}/>
-                    <Route exact path="/oddaj-rzeczy" element={<FormSuspense />}/>
+                    <Route element={<ProtectedRoute email={email}/>}>
+                        <Route exact path="/wylogowano" element={<SignOutSuspense />}/>
+                        <Route exact path="/oddaj-rzeczy" element={<FormSuspense />}/>
+                    </Route>
                 </Routes>
             </Suspense>
         </BrowserRouter>
